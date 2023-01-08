040dc20c3df2f994bf00476999238d9c527d6084
jcdenton.eth <jc@beb.xyz>
2023-01-07 19:54:03 -0800

    feat: create a schema folder with .gql files

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { loadSchema } from "@graphql-tools/load";
+import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
 
-// Follow Apollo guide at https://www.apollographql.com/docs/apollo-server/getting-started
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
+const typeDefs = await loadSchema("./schema/*.gql", {
+  loaders: [new GraphQLFileLoader()],
+});
 
 const books = [
   {
