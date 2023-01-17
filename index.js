b423b3fa972d6f14110776a53ee70132fc300581
jcdenton.eth <jc@beb.xyz>
2023-01-17 14:09:02 -0800

    feat: Add DimensionSchema and other schemas tied to it

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { loadSchema } from "@graphql-tools/load";
 import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
 import * as dotenv from "dotenv";
 import { connectDB } from "./helpers/connectdb.js";
-import * as _schemas from "./schema/schema.js";
+import * as _schemas from "./schema/main.js";
 
 dotenv.config();
 
--- schema/main.js
+++ schema/main.js
@@ -82,7 +82,7 @@ export const RichEmbedSchema = mongoose.Schema(
   { timestamps: true }
 );
 
-const RichContentBlockSchema = mongoose.Schema({
+export const RichContentBlockSchema = mongoose.Schema({
   blockType: {
     type: String,
     enum: ["IMAGE", "LINK", "RICH_EMBED", "COLLECTION", "POST"],
@@ -94,7 +94,203 @@ const RichContentBlockSchema = mongoose.Schema({
   },
 });
 
-const RichContentSchema = mongoose.Schema({
+export const PermissionSchema = mongoose.Schema(
+  {
+    description: ContentSchema,
+    name: { type: String, required: true },
+    community: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      index: true,
+      ref: "Dimension",
+    },
+    uniqueIdentifier: { type: String, index: true },
+    editable: { type: Boolean, default: false },
+    bitwiseFlag: { type: String, required: true, index: true },
+    bitwisePosition: { type: Number, required: true, index: true },
+  },
+  { timestamps: true }
+);
+
+export const PermissionOverwriteSchema = mongoose.Schema(
+  {
+    objectTypeId: {
+      type: mongoose.Schema.Types.ObjectId,
+      index: true,
+      required: true,
+    },
+    objectType: { type: Number, index: true, required: true },
+    allowedPermissionString: { type: String },
+    deniedPermissionString: { type: String },
+  },
+  { timestamps: true }
+);
+
+export const RichContentSchema = mongoose.Schema({
   content: ContentSchema,
   blocks: [RichContentBlockSchema],
 });
+
+export const ChannelSchema = mongoose.Schema(
+  {
+    description: ContentSchema,
+    name: { type: String, required: true },
+    slug: { type: String, index: true },
+    community: {
+      type: mongoose.Schema.Types.ObjectId,
+      index: true,
+      ref: "Dimension",
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      index: true,
+      ref: "Address",
+    },
+    position: {
+      type: Number,
+      default: Number.MAX_SAFE_INTEGER,
+    },
+    icon: {
+      type: mongoose.Schema.Types.ObjectId,
+      index: true,
+      ref: "Image",
+    },
+    permissionsOverwrite: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        index: true,
+        ref: "PermissionOverwrite",
+      },
+    ],
+    isHidden: { type: Boolean, default: false, index: true },
+  },
+  { timestamps: true }
+);
+
+export const DimensionSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    bio: {
+      type: contentSchema,
+    },
+    image: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Image",
+    },
+    bannerImage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Image",
+    },
+    bebdomain: {
+      type: String,
+      index: true,
+    },
+    host: {
+      type: String,
+      index: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Address",
+    },
+    permissions: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Permission",
+        index: true,
+      },
+    ],
+    roles: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Role",
+        index: true,
+      },
+    ],
+    channels: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Channel",
+        index: true,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const RoleSchema = mongoose.Schema(
+  {
+    description: ContentSchema,
+    name: { type: String, required: true },
+    slug: { type: String, required: true, index: true },
+    community: {
+      type: mongoose.Schema.Types.ObjectId,
+      index: true,
+      ref: "Dimension",
+    },
+    icon: {
+      type: mongoose.Schema.Types.ObjectId,
+      index: true,
+      ref: "Image",
+    },
+    editable: { type: Boolean, default: false },
+    color: { type: String },
+    position: { type: Number },
+    permissionString: { type: String },
+    isHidden: { type: Boolean, default: false, index: true },
+  },
+  { timestamps: true }
+);
+
+export const AddressDimensionRoleSchema = mongoose.Schema(
+  {
+    role: {
+      type: mongoose.Schema.Types.ObjectId,
+      index: true,
+      ref: "Role",
+      required: true,
+    },
+    accountCommunity: {
+      type: mongoose.Schema.Types.ObjectId,
+      index: true,
+      ref: "AddressDimension",
+      required: true,
+    },
+    isValid: { type: Boolean, default: true, index: true },
+  },
+  { timestamps: true }
+);
+
+export const AddressDimensionSchema = mongoose.Schema(
+  {
+    account: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Address",
+      index: true,
+    },
+    dimension: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Dimension",
+      index: true,
+    },
+    joined: {
+      type: Boolean,
+      default: false,
+      index: true,
+    },
+    roles: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "AddressDimensionRole",
+        index: true,
+      },
+    ],
+    lastSeen: { type: Date, default: new Date(), index: true },
+    joinedDate: { type: Date, default: new Date(), index: true },
+  },
+  { timestamps: true }
+);
