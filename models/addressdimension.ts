a1575b2d2dd7ad4453461163047e1f7fec9543a8
jcdenton.eth <jc@beb.xyz>
2023-01-28 13:34:20 -0800

    feat: add model logic for AddressDimensionClass

--- models/addressdimension.ts
+++ models/addressdimension.ts
@@ -1,7 +1,40 @@
 import mongoose from "mongoose";
 import { AddressDimensionSchema } from "../schema/main.js";
 
-class AddressDimensionClass extends mongoose.Model {}
+class AddressDimensionClass extends mongoose.Model {
+  static async _existingAddressDimension({
+    addressId,
+    dimensionId,
+  }: {
+    addressId: string;
+    dimensionId: string;
+  }) {
+    if (!addressId || !dimensionId) return null;
+    return this.findOne({ address: addressId, dimension: dimensionId });
+  }
+
+  static async updateOrCreate({
+    addressId,
+    dimensionId,
+    ...props
+  }: {
+    addressId: string;
+    dimensionId: string;
+  }): Promise<AddressDimensionClass> {
+    const found = await this._existingAddressDimension({
+      addressId,
+      dimensionId,
+    });
+    if (found) {
+      return found;
+    }
+    return await this.create({
+      address: addressId,
+      dimension: dimensionId,
+      ...props,
+    });
+  }
+}
 
 AddressDimensionSchema.loadClass(AddressDimensionClass);
 
