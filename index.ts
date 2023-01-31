ebf2ceb0a7e112034d9cb8a8ce31c9679a818743
jcdenton.eth <jc@beb.xyz>
2023-01-31 10:54:47 -0800

    feat: cleanup kad dht experiment

--- index.ts
+++ index.ts
@@ -5,13 +5,6 @@ import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
 import * as dotenv from "dotenv";
 import { connectDB } from "./helpers/connectdb.js";
 import { resolvers } from "./resolvers/index.js";
-import { createLibp2p } from "libp2p";
-import { tcp } from "@libp2p/tcp";
-import { noise } from "@chainsafe/libp2p-noise";
-import { kadDHT } from "@libp2p/kad-dht";
-import { CID } from "multiformats/cid";
-import * as json from "multiformats/codecs/json";
-import { sha256 } from "multiformats/hashes/sha2";
 
 dotenv.config();
 
@@ -32,33 +25,3 @@ const { url } = await startStandaloneServer(server, {
 });
 
 console.log(`🚀 Server ready at: ${url}`);
-
-const bytes = json.encode({
-  identifier: "dimensions@0.0.1",
-  name: "bebverse/dimensions",
-});
-const hash = await sha256.digest(bytes);
-const cid = CID.create(1, json.code, hash);
-
-const node = await createLibp2p({
-  addresses: {
-    listen: ["/ip4/0.0.0.0/tcp/0"],
-  },
-  dht: kadDHT({
-    kBucketSize: Number.MAX_SAFE_INTEGER,
-    protocolPrefix: "/dimension",
-    clientMode: false,
-  }),
-  transports: [tcp()],
-  connectionEncryption: [noise()],
-});
-node.dht.provide(cid);
-await node.start();
-
-for await (const event of node.dht.findProviders(cid, {
-  queryFuncTimeout: 1000,
-})) {
-  console.info(event);
-}
-
-export { server };
