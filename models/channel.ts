510d8c86e50708f5b9e38f91323424ab6a02d50a
jcdenton.eth <jc@beb.xyz>
2023-02-03 11:39:08 -0800

    feat: add buildMatchQuery, generateUniqueSlug to Channel

--- models/channel.ts
+++ models/channel.ts
@@ -1,7 +1,57 @@
 import mongoose from "mongoose";
 import { ChannelSchema } from "../schema/main.js";
 
-class ChannelClass extends mongoose.Model {}
+class ChannelClass extends mongoose.Model {
+  static async _generateUniqueSlug({
+    name,
+    index = 0,
+  }: {
+    name: string;
+    index?: number;
+  }): Promise<string> {
+    if (index > 10) throw new Error("Cannot generate unique slug");
+    /** generate random 4 numbers */
+    const random = Math.floor(1000 + Math.random() * 9000);
+    const slug = `${name.toLowerCase().replace(/\s/g, "-")}-${random}`;
+    const found = await this.exists({ slug });
+    if (found)
+      return await this._generateUniqueSlug({ name, index: index + 1 });
+    return slug;
+  }
+
+  static _buildMatchQuery({
+    filters,
+  }: {
+    filters: { communityId?: string; onlyPublic: boolean };
+  }) {
+    let matchQuery = {};
+    if (filters.communityId) {
+      matchQuery = {
+        ...matchQuery,
+        community: new mongoose.Types.ObjectId(filters.communityId),
+      };
+    }
+    if (filters.onlyPublic) {
+      matchQuery = {
+        ...matchQuery,
+        $or: [
+          {
+            recipients: {
+              $exists: false,
+            },
+          },
+          {
+            recipients: {
+              $size: 0,
+            },
+          },
+        ],
+      };
+    }
+
+    return matchQuery;
+  }
+}
 
 ChannelSchema.loadClass(ChannelClass);
 
