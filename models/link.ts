a1b31f16a458dd0241dc240ae7246a808de33211
jcdenton.eth <jc@beb.xyz>
2023-02-03 14:39:19 -0800

    feat: add link scraping to Link

--- models/link.ts
+++ models/link.ts
@@ -1,8 +1,25 @@
 import mongoose from "mongoose";
 import { LinkSchema } from "../schema/main.js";
-
 import axios from "axios";
 import axiosRetry from "axios-retry";
+import _metascraper from "metascraper";
+import metascraperDescription from "metascraper-description";
+import metascraperImage from "metascraper-image";
+import metascraperLogo from "metascraper-logo";
+import metascraperTitle from "metascraper-title";
+import metascraperUrl from "metascraper-url";
+import metascraperIframe from "metascraper-iframe";
+import { cleanIframeHtml } from "../helpers/html.js";
+
+const metascraper = _metascraper([
+  metascraperDescription(),
+  metascraperImage(),
+  metascraperLogo(),
+  metascraperTitle(),
+  metascraperUrl(),
+  metascraperIframe(),
+]);
+
 axiosRetry(axios, { retries: 3 });
 
 const TIMEOUT = 10000;
@@ -29,6 +46,76 @@ class LinkClass extends mongoose.Model {
       html = data;
     }
     return html;
+  }
+
+  static async createRichLink({
+    url: targetUrl,
+    callback,
+    onError,
+  }: {
+    url: string;
+    callback?: (link: LinkClass) => Promise<LinkClass>;
+    onError?: (e: Error) => void;
+  }): Promise<LinkClass | null> {
+    if (!targetUrl) return null;
+    if (targetUrl.includes(".pdf")) {
+      // Skip PDFs due to segfaults on large files
+      return callback?.(null) || null;
+    }
+    if (targetUrl.includes(".dmg")) {
+      // Skip DMGs due to segfaults on large files
+      return callback?.(null) || null;
+    }
+    if (targetUrl.includes(".zip")) {
+      // Skip ZIPs due to segfaults on large files
+      return callback?.(null) || null;
+    }
+
+    try {
+      const html = await LinkClass.getHtml(targetUrl);
+      const link = new LinkClass();
+      callback?.(link); // in case we want to async scrape the link
+
+      const {
+        description,
+        image,
+        title,
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        logo,
+        url: cleanUrl,
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        iframe: rawIframe,
+      } = await metascraper({
+        html,
+        url: targetUrl,
+      });
+
+      /**@TODO we only support twitter iframe for now */
+      const iframe = rawIframe
+        ? cleanUrl?.indexOf("twitter") !== -1
+          ? cleanIframeHtml(rawIframe)
+          : null
+        : null;
+
+      link.url = cleanUrl;
+      link.title = title;
+      link.description = description;
+      link.image = image;
+      link.logo = logo;
+      link.iframe = iframe;
+
+      return await link.save();
+    } catch (e: unknown) {
+      console.error(e);
+      if (e instanceof Error) {
+        onError?.(e);
+      } else {
+        onError?.(new Error("Unknown error"));
+      }
+      return null;
+    }
   }
 }
 
