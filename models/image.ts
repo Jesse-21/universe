8f2f9a44cb6b8fadd7e285d9176470cd92d6d105
jcdenton.eth <jc@beb.xyz>
2023-02-03 13:07:14 -0800

    feat: add uploadImage to Image

--- models/image.ts
+++ models/image.ts
@@ -1,7 +1,51 @@
 import mongoose from "mongoose";
 import { ImageSchema } from "../schema/main.js";
+import axios from "axios";
+import FormData from "form-data";
+import fs from "fs";
 
-class ImageClass extends mongoose.Model {}
+class ImageClass extends mongoose.Model {
+  static async uploadImage({
+    image,
+  }: {
+    image: string | { filepath: string; newFilename: string };
+  }) {
+    try {
+      const form = new FormData();
+      if (image instanceof String || typeof image === "string") {
+        form.append("image", image);
+      } else {
+        form.append("image", fs.createReadStream(image.filepath));
+        form.append("type", "file");
+        form.append("name", image.newFilename);
+      }
+
+      const response = await axios.post("https://api.imgur.com/3/image", form, {
+        headers: {
+          ...form.getHeaders(),
+          Authorization: `Client-ID ${process.env.IMGUR_CLIENT_ID}`,
+        },
+      });
+      if (response?.data?.success) {
+        const image = await Image.create({
+          src: response.data.data.link,
+          name: response.data.data.name,
+          isVerified: false,
+        });
+        return image;
+      } else {
+        throw new Error("Imgur API error");
+      }
+    } catch (e: unknown) {
+      console.error(e);
+      if (e instanceof Error) {
+        throw Error(e.message);
+      } else {
+        throw Error("Imgur API error");
+      }
+    }
+  }
+}
 
 ImageSchema.loadClass(ImageClass);
 
