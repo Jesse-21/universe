856c95ae1fb07cd009947be9278c4bf848f1afa2
jcdenton.eth <jc@beb.xyz>
2023-02-03 13:55:58 -0800

    feat: add getHtml to LinkClass

--- models/link.ts
+++ models/link.ts
@@ -1,7 +1,36 @@
 import mongoose from "mongoose";
 import { LinkSchema } from "../schema/main.js";
 
-class LinkClass extends mongoose.Model {}
+import axios from "axios";
+import axiosRetry from "axios-retry";
+axiosRetry(axios, { retries: 3 });
+
+const TIMEOUT = 10000;
+
+class LinkClass extends mongoose.Model {
+  static async getHtml(targetUrl: string): Promise<string> {
+    let html;
+    if (targetUrl.includes("twitter.com")) {
+      const { data } = await axios.get(
+        `https://publish.twitter.com/oembed?url=${targetUrl}`,
+        {
+          timeout: TIMEOUT,
+        }
+      );
+      html = data.html;
+    } else {
+      const { data } = await axios.get(targetUrl, {
+        headers: {
+          Accept:
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        },
+        timeout: TIMEOUT,
+      });
+      html = data;
+    }
+    return html;
+  }
+}
 
 LinkSchema.loadClass(LinkClass);
 
