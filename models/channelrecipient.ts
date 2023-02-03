dcd863eb4ad2b60312d82f36898dd8ecd7a5b155
jcdenton.eth <jc@beb.xyz>
2023-02-03 12:26:39 -0800

    feat: add _buildMatchQuery, findAndSort to ChannelRecipientClass

--- models/channelrecipient.ts
+++ models/channelrecipient.ts
@@ -1,7 +1,74 @@
 import mongoose from "mongoose";
 import { ChannelRecipientSchema } from "../schema/main.js";
 
-class ChannelRecipientClass extends mongoose.Model {}
+class ChannelRecipientClass extends mongoose.Model {
+  static _buildMatchQuery({
+    filters,
+  }: {
+    filters: {
+      recipientIds?: string[];
+      recipientType?: string;
+      communityId?: string;
+    };
+  }): mongoose.FilterQuery<ChannelRecipientClass> {
+    let matchQuery = {};
+    if (filters.recipientIds && filters.recipientIds.length) {
+      matchQuery = {
+        ...matchQuery,
+        recipientId: {
+          $in: filters.recipientIds.map(
+            (id) => new mongoose.Types.ObjectId(id)
+          ),
+        },
+      };
+    }
+    if (filters.recipientType) {
+      matchQuery = {
+        ...matchQuery,
+        recipientType: filters.recipientType,
+      };
+    }
+
+    if (filters.communityId) {
+      matchQuery = {
+        ...matchQuery,
+        community: new mongoose.Types.ObjectId(filters.communityId),
+      };
+    }
+
+    return matchQuery;
+  }
+
+  static async findAndSort({
+    limit = 20,
+    offset = 0,
+    filters = {},
+    sort = "createdAt",
+  }: {
+    limit?: number;
+    offset?: number;
+    filters?: {
+      recipientIds?: string[];
+      recipientType?: string;
+      communityId?: string;
+    };
+    sort?: string;
+  }) {
+    const matchQuery = this._buildMatchQuery({ filters });
+    const $sort =
+      sort[0] === "-" ? { [sort.slice(1)]: -1, _id: 1 } : { [sort]: 1, _id: 1 };
+
+    const channelRecipients = await this.aggregate([
+      { $match: matchQuery },
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      { $sort: $sort },
+      { $skip: offset },
+      { $limit: limit },
+    ]);
+    return channelRecipients;
+  }
+}
 
 ChannelRecipientSchema.loadClass(ChannelRecipientClass);
 
