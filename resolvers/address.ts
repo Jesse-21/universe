259bcbc5ec30e1d0a3d6f79a7a47390afec9e089
jcdenton.eth <jc@beb.xyz>
2023-02-05 15:17:32 -0800

    feat: add nonce to Address resolver

--- resolvers/address.ts
+++ resolvers/address.ts
@@ -1,1 +1,10 @@
-export const resolvers = {};
+import { AddressNonce } from "../models/addressnonce.js";
+
+export const resolvers = {
+  Address: {
+    nonce: async (parent: { _id: string }) => {
+      const addressNonce = await AddressNonce.findOne({ account: parent._id });
+      return addressNonce;
+    },
+  },
+};
