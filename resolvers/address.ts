5fe149761c3e3d07049638a56e44e2ec9b53261a
jcdenton.eth <jc@beb.xyz>
2023-02-09 20:16:41 -0800

    feat: correct types on Address resolver

--- resolvers/address.ts
+++ resolvers/address.ts
@@ -1,14 +1,15 @@
 import { AddressNonce } from "../models/addressnonce.js";
 import { AddressDimension } from "../models/addressdimension.js";
+import { IAddress } from "../schema/interfaces.js";
 
 export const resolvers = {
   Address: {
-    nonce: async (parent: { _id: string }) => {
+    nonce: async (parent: IAddress) => {
       const addressNonce = await AddressNonce.findOne({ account: parent._id });
       return addressNonce;
     },
 
-    addressDimensions: async (parent: { _id: string }) => {
+    addressDimensions: async (parent: IAddress) => {
       const addressDimensions = await AddressDimension.find({
         address: parent._id,
       });
