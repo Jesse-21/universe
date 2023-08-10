33ac3ba4e2a72467e119a1a372cb3cfa783ea155
jcdenton.eth <jc@beb.xyz>
2023-08-09 22:27:13 -0700

    feat: increment version to 2.1.28

--- graphql/resolvers/AccountInventory.js
+++ graphql/resolvers/AccountInventory.js
@@ -4,6 +4,16 @@ const {
 const { Account } = require("../../models/Account");
 const resolvers = {
   AccountInventoryItem: {
+    __resolveType(parent) {
+      switch (parent.rewardType) {
+        case "ASSET_3D":
+          return "Asset3DUnion";
+        case "IMAGE":
+          return "ImageUnion";
+        default:
+          return "Asset3DUnion";
+      }
+    },
     account: async (parent) => {
       const account = await Account.findById(parent.account);
       return account;
@@ -18,13 +28,13 @@ const resolvers = {
       if (parent.type === "ASSET_3D") {
         return {
           _id: parent.rewardId,
-          type: parent.type,
+          type: parent.rewardType,
           asset3D: reward,
         };
       } else if (parent.type === "IMAGE") {
         return {
           _id: parent.rewardId,
-          type: parent.type,
+          type: parent.rewardType,
           image: reward,
         };
       } else {
