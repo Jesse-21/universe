d801ce4ee386bb26258ffa89408754aebab212cc
jcdenton.eth <jc@beb.xyz>
2023-08-11 05:58:43 -0700

    feat: increment version to 2.1.31

--- services/RegistrarService.js
+++ services/RegistrarService.js
@@ -61,7 +61,21 @@ class RegistrarService {
     if (!domain) return null;
     const tokenId = this.getTokenIdFromLabel(domain);
     try {
+      const CacheService = new _CacheService();
+      const cachedOwner = await CacheService.get({
+        key: `RegistrarService.getOwner`,
+        params: { domain },
+      });
+      if (cachedOwner) return cachedOwner;
       const owner = await this.registrar.ownerOf(tokenId);
+      if (owner) {
+        CacheService.set({
+          key: `RegistrarService.getOwner`,
+          params: { domain },
+          value: owner,
+          expiresAt: new Date(Date.now() + 1000 * 60 * 15), // 15 minute cache,
+        });
+      }
       return owner;
     } catch (e) {
       Sentry.captureException(e);
@@ -100,8 +114,7 @@ class RegistrarService {
           key: `RegistrarService.expiresAt`,
           params: { domain },
           value: nameDuration.toString(),
-          // 1 day
-          expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24), // 1 day from now,
+          expiresAt: new Date(Date.now() + 1000 * 60 * 15), // 15 minute cache,
         });
       }
       return nameDuration?.toString();
