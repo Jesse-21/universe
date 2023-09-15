2069bd09ba5e8024561fbabdd0f28b9b28b4b1a2
jcdenton.eth <jc@beb.xyz>
2023-09-15 02:57:34 -0700

    feat: increment version to 2.1.125

--- helpers/farcaster.js
+++ helpers/farcaster.js
@@ -1132,7 +1132,7 @@ const getFarcasterFeed = async ({
 
   // determine time 24 hours ago
   const oneDayAgo = Date.now() - 24 * 60 * 60 * 1000;
-  const threshold = 175;
+  const threshold = 200;
 
   // create a basic query for casts
   let query = {
