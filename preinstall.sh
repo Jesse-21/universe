4256bc1f3a89be5dcc919e06a89189b20523ca1d
jcdenton.eth <jc@beb.xyz>
2023-09-21 11:04:39 -0700

    feat: increment version to 2.1.152

--- preinstall.sh
+++ preinstall.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check if brew is installed
+if ! command -v brew &> /dev/null; then
+    echo "Homebrew not found. Please install it first."
+    exit 0
+fi
+
 # Check if memcached is installed
 if ! command -v memcached &> /dev/null; then
     echo "Memcached not found. Installing via Homebrew..."
