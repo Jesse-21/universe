35da0b902a7d45368de11555a4e74492673a0e69
jcdenton.eth <jc@beb.xyz>
2023-11-05 14:44:16 -0800

    feat: increment version to 2.2.38

--- helpers/marketplace.js
+++ helpers/marketplace.js
@@ -5,7 +5,7 @@ const { abi: idRegistrarAbi } = require("../helpers/abi/id-registrar");
 const dev = () => {
   return {
     WETH_ADDRESS: "0x32307adfFE088e383AFAa721b06436aDaBA47DBE",
-    FID_MARKETPLACE_V1_ADDRESS: "0xab4b492301bd67b6769e8129e06a2743e975559d",
+    FID_MARKETPLACE_V1_ADDRESS: "0x3c581555eb9bbb69df165678abcf6c7c74496a28",
     FID_MARKETPLACE_PROXY_V1_ABI: MarketplaceV1Proxy.abi,
     FID_MARKETPLACE_V1_ABI: MarketplaceV1.abi,
     FID_ADDRESS: "0x43Be7849F724E7CA7D56BDeCeCb277adb99b8A83",
