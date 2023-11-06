89d5feefecb32cc5f19c01d668fc07183e970b0a
jcdenton.eth <jc@beb.xyz>
2023-11-06 06:32:39 -0800

    feat: increment version to 2.2.45

--- connectmemcached.js
+++ connectmemcached.js
@@ -8,7 +8,7 @@ module.exports = {
       client = new MemcacheClient({
         server: {
           server: process.env.MEMCACHED_URL || "localhost:11211",
-          maxConnections: 100,
+          maxConnections: 10,
         },
       });
     }
