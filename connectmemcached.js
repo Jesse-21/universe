f9f46cf7b6245cb3fe77070ab8401dd2ce617da8
jcdenton.eth <jc@beb.xyz>
2023-11-06 06:27:20 -0800

    feat: increment version to 2.2.43

--- connectmemcached.js
+++ connectmemcached.js
@@ -1,14 +1,18 @@
 const { MemcacheClient } = require("memcache-client");
 
+let client;
+
 module.exports = {
   getMemcachedClient: () => {
-    // always return a new client since this client is unstable when used in multiple threads
-    // Unstability: MemcachedConnection.receiveResult#188 Cannot set properties of undefined (setting 'error')
-    return new MemcacheClient({
-      server: {
-        server: process.env.MEMCACHED_URL || "localhost:11211",
-        maxConnections: 1,
-      },
-    });
+    if (!client) {
+      client = new MemcacheClient({
+        server: {
+          server: process.env.MEMCACHED_URL || "localhost:11211",
+          maxConnections: 10,
+        },
+      });
+    }
+
+    return client;
   },
 };
