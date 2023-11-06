4ed3465885de2b8bf729bc9f19bc713840bf0179
jcdenton.eth <jc@beb.xyz>
2023-11-06 06:36:41 -0800

    feat: increment version to 2.2.46

--- connectmemcached.js
+++ connectmemcached.js
@@ -8,7 +8,7 @@ module.exports = {
       client = new MemcacheClient({
         server: {
           server: process.env.MEMCACHED_URL || "localhost:11211",
-          maxConnections: 10,
+          maxConnections: 10, // With low maxConnections, we start getting `CLIENT_ERROR bad command line format` errors
         },
       });
     }
