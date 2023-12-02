e305180e8d72780e6477f771522980373dfd7039
jcdenton.eth <jc@beb.xyz>
2023-12-01 18:32:25 -0800

    feat: increment version to 2.3.9

--- helpers/validate-community-name.js
+++ helpers/validate-community-name.js
@@ -5,10 +5,11 @@ function isEncodedLabelhash(hash) {
   return hash.startsWith("[") && hash.endsWith("]") && hash.length === 66;
 }
 
-const validRegex = new RegExp("^[A-Za-z0-9-]+$");
+const validRegex = new RegExp("^[A-Za-z0-9-_]+$");
 
 function validateName(name) {
-  const nameArray = name.split(".");
+  const prefix = name.startsWith("op_") ? "op_" : "";
+  const nameArray = name.replace("op_", "").split(".");
   const hasEmptyLabels = nameArray.some((label) => label.length == 0);
   if (hasEmptyLabels) throw new Error("Domain cannot have empty labels");
   const normalizedArray = nameArray.map((label) => {
@@ -25,10 +26,12 @@ function validateName(name) {
   }
 
   if (!validRegex.test(_name.replace(".beb", "").replace(".cast", ""))) {
-    throw new Error("Domain cannot have invalid characters valid=(A-Za-z0-9_)");
+    throw new Error(
+      "Domain cannot have invalid characters valid=(A-Za-z0-9-_)"
+    );
   }
 
-  return _name;
+  return prefix + _name;
 }
 
 module.exports = { validateName };
